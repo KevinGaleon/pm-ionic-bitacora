a8a818c6c40fd58d50f9e2f8fd4440ceec77c248
Kevin Garzón <kevin_s_l@hotmail.com>
2022-08-30 22:01:37 -0500

    change sintaxis of function component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,35 +26,37 @@ import Settings from './pages/Settings/Settings';
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonTabs>
-          <IonRouterOutlet>
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/settings" component={Settings} />
-            <Route exact path="/">
-              <Redirect to="/home" />
-            </Route>
-          </IonRouterOutlet>
-          <IonTabBar slot='bottom'>
-            <IonTabButton tab='home' href='/home'>
-              <IonIcon icon={home} />
-            </IonTabButton>
-            <IonTabButton tab='settings' href='/settings'>
-              <IonIcon icon={person} />
-            </IonTabButton>
-          </IonTabBar>
-        </IonTabs>
-      </IonContent>
-    </IonReactRouter>
-  </IonApp>
-);
+const App = () => {
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <IonTabs>
+            <IonRouterOutlet>
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/settings" component={Settings} />
+              <Route exact path="/">
+                <Redirect to="/home" />
+              </Route>
+            </IonRouterOutlet>
+            <IonTabBar slot='bottom'>
+              <IonTabButton tab='home' href='/home'>
+                <IonIcon icon={home} />
+              </IonTabButton>
+              <IonTabButton tab='settings' href='/settings'>
+                <IonIcon icon={person} />
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
+        </IonContent>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
