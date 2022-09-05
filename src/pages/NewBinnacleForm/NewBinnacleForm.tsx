d1c4492f2a30a52e9d11e85965d11c30fdeddf09
Kevin Garzón <kevin_s_l@hotmail.com>
2022-09-05 11:06:15 -0500

    add create binnacle request

--- src/pages/NewBinnacleForm/NewBinnacleForm.tsx
+++ src/pages/NewBinnacleForm/NewBinnacleForm.tsx
@@ -1,13 +1,17 @@
 import React, { useRef, useState } from 'react'
-import { IonCol, IonContent, IonGrid, IonImg, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTextarea } from '@ionic/react';
-import { close, sendOutline } from 'ionicons/icons';
+import { IonCol, IonContent, IonGrid, IonIcon, IonImg, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTextarea } from '@ionic/react';
+import { close, sendOutline, trash } from 'ionicons/icons';
 import Header from '../../components/Header/Header';
 import { formatLocaleDate } from '../../utils/date';
 import { useHistory } from 'react-router';
+import { Binnacle } from '../../common/interfaces/binnacle';
+import { useReduxDispatch, useReduxSelector } from '../../redux/store';
+import { createBinnacle } from '../../redux/services/binnacle.services';
 
 const NewBinnacleForm = () => {
   const inputTitle = useRef<HTMLIonInputElement>(null);
   const inputDate = useRef<HTMLIonInputElement>(null);
+  const inputUrl = useRef<HTMLIonInputElement>(null);
   const inputDescription = useRef<HTMLIonTextareaElement>(null);
   const inputComment = useRef<HTMLIonTextareaElement>(null);
   const [photos, setPhotos] = useState([
@@ -19,10 +23,26 @@ const NewBinnacleForm = () => {
   const currentDate = formatLocaleDate(`${new Date()}`);
   const history = useHistory();
 
+  const { _id: binnacleGroupId } = useReduxSelector(state => state.binnacleGroup.binnacleGroupSelected);
+  const dispatch = useReduxDispatch();
+
+  const addPhoto = () => {
+    const url =  inputUrl.current?.value;
+    if (url) {
+      setPhotos([...photos, `${url}`]);
+      inputUrl.current!.value = '';
+    }
+  }
+
+  const removePhoto  = (url: string) => {
+    const newPhotos = photos.filter(img => url !== img);
+    setPhotos(newPhotos);
+  }
+
   const renderAddButton = () => {
     return (
       <IonCol size='4'>
-        <IonItem button onClick={() => console.log('TODO open alert to add new image')}>
+        <IonItem button onClick={addPhoto}>
           <IonLabel class='ion-text-center'>AGG IMAGEN</IonLabel>
         </IonItem>
       </IonCol>
@@ -37,6 +57,9 @@ const NewBinnacleForm = () => {
       cols.push(
         <IonCol size='4'>
           <IonImg src={photos[index]}></IonImg>
+          <IonItem button onClick={() => removePhoto(photos[index])}>
+            <IonIcon icon={trash} />
+          </IonItem>
         </IonCol>
       );
       if ((index + 1) % 3 === 0) {
@@ -58,6 +81,19 @@ const NewBinnacleForm = () => {
     return  rows;
   }
 
+  const onSubmit = () => {
+    const newBinnacle: Binnacle = {
+      binnacleGroupId: binnacleGroupId ?? '',
+      title: `${inputTitle.current?.value}`,
+      description: `${inputDescription.current?.value}`,
+      date: `${inputDate.current?.value}`,
+      comment: `${inputComment.current?.value}`,
+      photos,
+    }
+    dispatch(createBinnacle(newBinnacle));
+    goToBinnacleScreen();
+  }
+
   const goToBinnacleScreen = () => {
     history.replace('/binnacle');
   }
@@ -69,10 +105,7 @@ const NewBinnacleForm = () => {
         headerLeft={{ icon: close, onClick: () => history.goBack() }}
         headerRight={{
           icon: sendOutline,
-          onClick: () => { 
-            console.log('TODO crear nueva bitacora and dispatch response.data binacle selected');
-            goToBinnacleScreen();
-          }
+          onClick: onSubmit,
         }}
       />
       <IonContent>
@@ -106,6 +139,15 @@ const NewBinnacleForm = () => {
               <IonItem>
                 <IonLabel position="floating">Comentario</IonLabel>
                 <IonTextarea ref={inputComment} rows={4} />
+              </IonItem>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Url de imagen</IonLabel>
+                <IonInput ref={inputUrl} type="text" />
               </IonItem>
             </IonCol>
           </IonRow>
