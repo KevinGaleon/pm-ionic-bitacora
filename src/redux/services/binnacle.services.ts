a3ebec531c6d2ae64d6db277e0521af2ddde7341
Kevin Garzón <kevin_s_l@hotmail.com>
2022-09-04 21:23:20 -0500

    adjust design in binnacleScreen

--- src/redux/services/binnacle.services.ts
+++ src/redux/services/binnacle.services.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { BINNACLE_URL } from "../../common/config.api";
 import { Binnacle } from "../../common/interfaces/binnacle";
-import { setBinnacleList } from "../reducers/binnacleReducer/binnacle";
+import { setBinnacleList, setBinnacleSelected } from "../reducers/binnacleReducer/binnacle";
 
 export const fetchBinnacles = () =>async (dispatch:Function) => {
   try {
@@ -19,7 +19,8 @@ export const createBinnacle = (data: Binnacle) =>async (dispatch:Function) => {
   try {
     const response = await axios.post(BINNACLE_URL, data);
     if (response.data) {
-      console.log(response.data)
+      dispatch(setBinnacleSelected(response.data));
+      console.log(response.data);
     }
   } catch (error) {
     console.log(error);
